c5801097b19ab5f086d09df2b6567042e23c4976
Ramil Yusipov <usipoff@mail.ru>
2023-08-06 22:25:52 +0300

    Initial commit

---
+++ src/main/java/com/ramcom/chilladviser/ChilladviserApplication.java
@@ -0,0 +1,13 @@
+package com.ramcom.chilladviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChilladviserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChilladviserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramcom/chilladviser/ChilladviserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramcom.chilladviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChilladviserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
